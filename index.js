42c252c90a711d85c9e93ee05750d3d1282e882b
Rodrigo <rodrigodot42@gmail.com>
2018-11-28 20:53:13 -0200

    Simple routes

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 var http = require('http')
 
 var server = http.createServer( function(req, res) {
-  res.end('<html><body><h1>Hello World</h1></body></html>')
+
+  var route = req.url
+
+  console.log(req.url)
+
+  if(route === '/tecnologia') {
+    res.end('<html><body><h1>Tecnologia</h1></body></html>')
+  }
+  if(route === '/moda') {
+    res.end('<html><body><h1>Moda</h1></body></html>')
+  }
+  if(route === '/ciencia') {
+    res.end('<html><body><h1>Ciencia</h1></body></html>')
+  } else {
+    res.end('<html><body><h1>Hello World</h1></body></html>')
+  }
+
 })
 
 server.listen(3000)
